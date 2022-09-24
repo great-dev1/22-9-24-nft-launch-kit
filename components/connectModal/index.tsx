b53be3bddb7615ab559b01179edc0e8d03afb95f
great-dev1 <twentytwentyaugust@outlook.com>
2022-09-24 04:09:55 -0300

    Integrate wallet connections

--- components/connectModal/index.tsx
+++ components/connectModal/index.tsx
@@ -4,6 +4,26 @@ import BaseModal, { BaseModalProps } from "components/baseModal";
 import MatamaskIcon from "public/assets/metamask.svg";
 import WalletConnectIcon from "public/assets/wallet_connect.svg";
 import CoinbaseIcon from "public/assets/coinbase.svg";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { useWeb3React } from "@web3-react/core";
+
+const Injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42]
+});
+
+const CoinbaseWallet = new WalletLinkConnector({
+  url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+  appName: "Web3-react Demo",
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+const WalletConnect = new WalletConnectConnector({
+  rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+  bridge: "https://bridge.walletconnect.org",
+  qrcode: true,
+});
 
 type ConnectModalProps = {
   modalTitle: string;
@@ -11,6 +31,8 @@ type ConnectModalProps = {
 } & BaseModalProps;
 
 const ConnectModal: FC<ConnectModalProps> = ({ open, onClose, modalTitle }) => {
+  const { activate, deactivate } = useWeb3React();
+
   return (
     <BaseModal open={open} onClose={onClose} title={modalTitle}>
       <p className="text-sm text-gray-500 w-80">
@@ -18,7 +40,10 @@ const ConnectModal: FC<ConnectModalProps> = ({ open, onClose, modalTitle }) => {
       </p>
       <div className=" py-2">
         <div className="mt-4 relative">
-          <button className="w-full big-btn bg-gray-100 gap-2 !justify-start">
+          <button className="w-full big-btn bg-gray-100 gap-2 !justify-start" onClick={() => {
+            activate(Injected);
+            onClose();
+          }}>
             <Image src={MatamaskIcon} alt={"metamask"} width={24} height={24} />
             MetaMask
           </button>
@@ -27,13 +52,19 @@ const ConnectModal: FC<ConnectModalProps> = ({ open, onClose, modalTitle }) => {
           </span>
         </div>
         <div className="mt-4">
-          <button className="w-full big-btn bg-gray-100 gap-2 !justify-start">
+          <button className="w-full big-btn bg-gray-100 gap-2 !justify-start" onClick={() => {
+            activate(CoinbaseWallet);
+            onClose();
+          }}>
             <Image src={CoinbaseIcon} alt={"coinbase"} width={24} height={24} />
             Coinbase Wallet
           </button>
         </div>
         <div className="mt-4">
-          <button className="w-full big-btn bg-gray-100 gap-2 !justify-start">
+          <button className="w-full big-btn bg-gray-100 gap-2 !justify-start" onClick={() => {
+            activate(WalletConnect);
+            onClose();
+          }}>
             <Image src={WalletConnectIcon} alt={"wallet connect"} width={24} height={24} />
             WalletConnect
           </button>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import MainLayout from "components/mainLayout";
 
+function getLibrary(provider) {
+  return new Web3Provider(provider);
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </Web3ReactProvider>
   );
 };
 
